0d713f593f186cecbbf2ec4005883d6f34000db0
Cameron King <cking@ckxng.com>
2014-11-03 03:58:31 +0000

    jsbeautify app.js

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var port = process.env.PORT || 3000,
     http = require("http"),
     fs = require("fs"),
     html = fs.readFileSync("index.html");
-    
+
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send(html);
+app.set('title', 'imgawsome');
+
+app.get('/', function(req, res) {
+    res.send(html);
 });
 
-app.post('/', function(req, res){
- //do stuff that i've lost, so lets learn how to version control
+app.post('/', function(req, res) {
+    //do stuff that i've lost, so lets learn how to version control
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
