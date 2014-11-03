86feb579b530b9e6338bb449e95642ae42569bc0
Ian Campbell <miniatureian@gmail.com>
2014-11-03 03:04:53 +0000

    added require express

--- app.js
+++ app.js
@@ -2,26 +2,20 @@ var port = process.env.PORT || 3000,
     http = require("http"),
     fs = require("fs"),
     html = fs.readFileSync("index.html");
+    
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function (request, response) {
-    if (request.method == 'POST') {
-        request.on('data', function(chunk) {
-            fs.appendFile("/tmp/sample-app.log", chunk.toString() + "\n", function (err) {});
-        });
-        
-        request.on('end', function() {
-            response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-            response.end();
-        });
-    } else {
-        response.writeHead(200);
-        response.write(html);
-        response.end();
-    }
+app.get('/', function(req, res){
+  res.send(html);
+});
+
+app.post('/', function(req, res){
+ //do stuff that i've lost, so lets learn how to version control
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+app.listen(3000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:" + port + "/");
